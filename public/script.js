69ccdb9bcd67c1e6dd9fd569a5318332a3dbfa18
Pessoadomundo <gabrieljotalizardo@gmail.com>
2022-02-19 17:40:41 -0300

    first

---
+++ public/script.js
@@ -0,0 +1,366 @@
+///////////////////////////////////////////////////////////////
+const socket = io('https://af62-2804-14c-5b95-8d6d-2c36-7ae8-e80-7d07.sa.ngrok.io')
+
+var user 
+
+const foods = [{"name": "Filé Mignon", "preco":25, "id":0, "img": "comida.jpg"}, {"name": "Picanha", "preco":30, "id":1, "img": "comida2.png"}]
+
+let trans = document.getElementById("trans")
+let tela1 = document.getElementById("tela1")
+let tela2 = document.getElementById("tela2")
+let tela3 = document.getElementById("tela3")
+
+
+let loginTitle = document.getElementById("loginTitle")
+let email = document.getElementById("email")
+let senha = document.getElementById("senha")
+let login = document.getElementById("enviar")
+let registrar = document.getElementById("registrar")
+let result = document.getElementById("result")
+let tutorial = document.getElementById("tutorial")
+
+let container1 = document.getElementById("container")
+let container2 = document.getElementById("container2")
+let container3 = document.getElementById("container3")
+
+let plus = document.getElementById("plus")
+let backFromAdd = document.getElementById("backFromAdd")
+
+let bag = document.getElementById("bag")
+let carrinho = document.getElementById("carrinho")
+let perfil = document.getElementById("perfil")
+
+let addMoney = document.getElementById("addMoney")
+let pagamentoDiv = document.getElementById("pagamentoDiv")
+
+let divComidas = document.getElementById("comidas")
+
+let payNow = document.getElementById("payNow")
+let useBalance = document.getElementById("useBalance")
+
+let itensCarrinho = document.getElementById("itensCarrinho")
+let pendingItems = document.getElementById("pendingItems")
+
+let saldo = document.getElementById("saldo")
+
+let plusSign = document.getElementsByClassName("plusSign")
+let minusSign = document.getElementsByClassName("minusSign")
+
+let totalValue = document.getElementById("totalValue")
+
+let notificationDiv = document.getElementById("notificationDiv")
+
+let currentMoney = document.getElementById("currentMoney")
+let copySymbol = document.getElementById("copySymbol")
+
+let mode = 1
+
+function changePage(page){
+    if(page==2){
+        tela1.style.display = "none"
+        tela2.style.display = "block"
+        tela3.style.display = "none"
+        
+    }
+    if(page==3){
+        tela1.style.display = "none"
+        tela2.style.display = "none"
+        tela3.style.display = "block"
+    }
+}
+
+function changeMenu(menu){
+    if(menu==1){
+        container1.style.display = "block"
+        container2.style.display = "none"
+        container3.style.display = "none"
+    }else if(menu==2){
+        container1.style.display = "none"
+        container2.style.display = "block"
+        container3.style.display = "none"
+    }else if(menu==3){
+        container1.style.display = "none"
+        container2.style.display = "none"
+        container3.style.display = "block"
+    }
+}
+
+function putFoods(){
+    divComidas.innerHTML = ""
+
+    foods.forEach(item => {
+        displayFood(item.id, 1, divComidas)
+    })
+}
+
+function updateCart(){
+    itensCarrinho.innerHTML = ""
+
+    let total = 0
+    for (let i = 0; i < user.cart.length; i++){
+        displayFood(user.cart[i].id, 2, itensCarrinho, user.cart[i].qtd)
+        total+=user.cart[i].qtd*foods[user.cart[i].id].preco
+    }
+
+    if(Number.isInteger(total)){
+        totalValue.innerHTML = "Total: R$"+total+",00"
+    }else{
+        totalValue.innerHTML = "Total: R$"+total
+    }
+}
+
+function updatePending(){
+    pendingItems.innerHTML = ""
+
+    for (let i = 0; i < user.pending.length; i++){
+        displayFood(user.pending[i].id, 3, pendingItems, user.pending[i].qtd)
+    }
+}
+
+function addToCart(id, amount){
+    socket.emit("addToCart", id, amount, user.id)
+    socket.emit("askUser", user.id)
+
+    if(amount>0){
+        displayAlert("Item adicionado ao carrinho com sucesso", true)
+    }
+    setTimeout(()=>{
+        updateCart()
+    },500)
+}
+
+function displayFood(id, type, elt, qtd=0){
+    let food = foods[id]
+
+
+    let eltComida = document.createElement("div")
+    eltComida.classList.add("comida")
+
+    let eltDivImgComida = document.createElement("div")
+    eltDivImgComida.classList.add("divImgComida")
+    
+    let eltImgComida = document.createElement("img")
+    eltImgComida.classList.add("imgComida")
+    eltImgComida.src = food.img
+    eltDivImgComida.appendChild(eltImgComida)
+    eltComida.appendChild(eltDivImgComida)
+
+    let eltDivInfoComida = document.createElement("div")
+    eltDivInfoComida.classList.add("divInfoComida")
+
+    let eltNomeComida = document.createElement("span")
+    eltNomeComida.classList.add("nomeComida")
+    eltNomeComida.innerHTML = food.name
+    eltDivInfoComida.appendChild(eltNomeComida)
+
+    if(type==1){
+        eltDivInfoComida.appendChild(document.createElement("br"))
+        let eltPreco = document.createElement("span")
+        eltPreco.classList.add("preco")
+        if(Number.isInteger(food.preco)){
+            eltPreco.innerHTML = "R$"+food.preco+",00"
+        }else{
+            eltPreco.innerHTML = "R$"+food.preco
+        }
+        eltDivInfoComida.appendChild(eltPreco)
+        eltDivInfoComida.appendChild(document.createElement("br"))
+        eltDivInfoComida.appendChild(document.createElement("br"))
+        let eltAddToCart = document.createElement("img")
+        eltAddToCart.src = "plusBlack.png"
+        eltAddToCart.classList.add("addToCart")
+        eltAddToCart.addEventListener("click", ()=>{addToCart(id, 1)})
+        eltDivInfoComida.appendChild(eltAddToCart)
+    }else if(type==2){
+        eltDivInfoComida.appendChild(document.createElement("br"))
+        let eltPreco = document.createElement("span")
+        eltPreco.classList.add("preco")
+        if(Number.isInteger(food.preco)){
+            eltPreco.innerHTML = "R$"+food.preco+",00"
+        }else{
+            eltPreco.innerHTML = "R$"+food.preco
+        }
+        eltDivInfoComida.appendChild(eltPreco)
+        eltDivInfoComida.appendChild(document.createElement("br"))
+
+        let amount = document.createElement("p")
+        amount.classList.add("amount")
+        amount.innerHTML = "Qtd: "+ qtd
+        eltDivInfoComida.appendChild(amount)
+        
+        let eltMinusSign = document.createElement("img")
+        eltMinusSign.src = "minusBlack.png"
+        eltMinusSign.classList.add("minusSign")
+        eltMinusSign.addEventListener("click", ()=>{addToCart(id, -1)})
+        eltDivInfoComida.appendChild(eltMinusSign)
+
+        let eltPlusSign = document.createElement("img")
+        eltPlusSign.src = "plusBlack.png"
+        eltPlusSign.classList.add("plusSign")
+        eltPlusSign.addEventListener("click", ()=>{addToCart(id, 1)})
+        eltDivInfoComida.appendChild(eltPlusSign)
+    }else if(type==3){
+        eltDivInfoComida.appendChild(document.createElement("br"))
+        let amount = document.createElement("p")
+        amount.classList.add("amount")
+        amount.innerHTML = "Qtd: "+ qtd
+        eltDivInfoComida.appendChild(amount)
+    }
+
+    eltComida.appendChild(eltDivInfoComida)
+
+    elt.appendChild(eltComida)
+}
+
+function displayBalance(){
+    if(Number.isInteger(user.saldo)){
+        saldo.innerHTML = "Saldo: R$"+user.saldo+",00"
+        currentMoney.innerHTML = "Saldo atual: R$"+user.saldo+",00"
+    }else{
+        saldo.innerHTML = "Saldo: R$"+user.saldo
+        currentMoney.innerHTML = "Saldo atual: R$"+user.saldo
+    }
+}
+
+function displayAlert(message, good){
+    notificationDiv.children[0].innerHTML = message
+    notificationDiv.style.display = "block"
+    if(good){
+        notificationDiv.style.backgroundColor = "lightgreen"
+        notificationDiv.style.borderColor = "lightgreen"
+    }else{
+        notificationDiv.style.backgroundColor = "#d66f6f"
+        notificationDiv.style.borderColor = "#d66f6f"
+    }
+    notificationDiv.style.animation = "notification 5s"
+    setTimeout(()=>{
+        notificationDiv.style.display = "none"
+    }, 5000)
+
+}
+
+login.addEventListener("click", ()=>{
+    if(mode==1){
+        socket.emit("login", email.value, senha.value)
+    }else if(mode==2){
+        socket.emit("registrar", email.value, senha.value)
+    }
+})
+
+registrar.addEventListener("click", ()=>{
+    if(mode==1){
+        mode=2
+        registrar.innerText = "Login"
+        login.innerText = "Registrar"
+        loginTitle.innerText = "Registrar"
+    }else if(mode==2){
+        mode=1
+        registrar.innerText = "Registrar"
+        login.innerText = "Login"
+        loginTitle.innerText = "Login"
+
+    }
+})
+
+plus.addEventListener("click", ()=>{
+    changePage(3)
+})
+backFromAdd.addEventListener("click", ()=>{
+    changePage(2)
+})
+
+bag.addEventListener("click", ()=>{
+    changeMenu(1)
+    bag.style.borderTop = "white 5px solid"
+    carrinho.style.borderTop = "black 0px solid"
+    perfil.style.borderTop = "black 0px solid"
+})
+
+carrinho.addEventListener("click", ()=>{
+    changeMenu(2)
+    bag.style.borderTop = "black 0px solid"
+    carrinho.style.borderTop = "white 5px solid"
+    perfil.style.borderTop = "black 5px solid"
+})
+
+perfil.addEventListener("click", ()=>{
+    changeMenu(3)
+    bag.style.borderTop = "black 5px solid"
+    carrinho.style.borderTop = "black 5px solid"
+    perfil.style.borderTop = "white 5px solid"
+})
+
+addMoney.addEventListener("click", ()=>{
+    pagamentoDiv.style.display = "block"
+})
+
+pagamentoDiv.addEventListener("click", ()=>{
+    pagamentoDiv.style.display = "none"
+})
+
+payNow.addEventListener("click", ()=>{
+    pagamentoDiv.style.display = "block"
+})
+
+useBalance.addEventListener("click", ()=>{
+    if(user.cart.length>0){
+        socket.emit("balancePayAsk", user.id)
+    }else{
+        displayAlert("Carrinho vazio", false)
+    }
+})
+copySymbol.addEventListener("click", ()=>{
+    navigator.clipboard.writeText("penis")
+    displayAlert("Código Pix copiado", true)
+})
+
+
+
+
+socket.on("loginState", data=>{
+    if(data.state == "Success"){
+        user = data.userInfo
+        trans.style.animation= "animation 2s";
+        putFoods()
+        updateCart()
+        updatePending()
+        setTimeout(()=>{
+            displayBalance()
+            changePage(2)
+            tela2.style.animation= "animation2 1s";
+        }, 2000)
+    }else if(data.state == "Fail"){
+        result.innerText = "Usuário e/ou senha incorretos"
+    }
+})
+
+
+socket.on("regState", data=>{
+    if(data.state == "Success"){
+        result.innerText = "Usuário criado com sucesso"
+        email.value = ""
+        senha.value = ""
+    }else if(data.state == "Fail"){
+        result.innerText = "Usuário já existente"
+    }
+})
+
+socket.on("updateUser", data=>{
+    user = data
+    console.log(user)
+})
+
+socket.on("balancePayResp", (state, saldo)=>{
+    if(state){
+        displayAlert("Pagamento feito com sucesso", true)
+        user.saldo = saldo
+    }else{
+        displayAlert("Saldo insuficiente", false)
+        user.saldo = saldo
+    }
+    setTimeout(()=>{
+        displayBalance()
+        updateCart()
+        updatePending()
+    },500)
+})
+
---
+++ server.js
@@ -0,0 +1,203 @@
+const fetch = require('node-fetch');
+
+const url = 'https://ws.sandbox.pagseguro.uol.com.br/v2/checkout?email=email&token-sandbox=B477D9D9745A470BBAE39AA9BACF0CBA&token=token-sandbox';
+const options = {
+  method: 'POST',
+  headers: {Accept: 'application/xml', 'Content-Type': 'application/json'}
+};
+
+fetch(url, options)
+  .then(res => res.json())
+  .then(json => console.log(json))
+  .catch(err => console.error('error:' + err));
+
+
+/*
+
+const _pix = require('faz-um-pix');
+
+const code = _pix.Pix("+5531988088186", "Gabriel Jota Lizardo", "Belo Horizonte", "10", "Penis");
+
+const qrcode = _pix.Pix("+5531988088186", "Gabriel Jota Lizardo", "Belo Horizonte", "10", "Penis", true);
+
+
+console.log(code)
+console.log(qrcode)
+
+*/
+
+
+const cons = require('consolidate');
+var express = require('express')
+var fs = require("fs")
+var app = express();
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+bodyParser = require("body-parser")
+
+
+app.use(express.static('./views'))
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: true}))
+
+let rawdata = fs.readFileSync('users.json')
+var users = JSON.parse(rawdata)
+const foods = [{"name": "Filé Mignon", "preco":25, "id":0, "img": "comida.jpg"}, {"name": "Picanha", "preco":30, "id":1, "img": "comida2.png"}]
+let orders = [{"nome":"File Mignon", "qtd": 0}, {"nome":"Picanha", "qtd": 0}]
+
+
+function getUserIndex(id){
+  let index = 0
+  let i = 0 
+  users.forEach(user => {
+    if(user.id == id){
+      index = i
+    }
+    i++
+  })
+  return index
+}
+
+function getAllOrder(){
+  orders = [{"nome":"File Mignon", "qtd": 0}, {"nome":"Picanha", "qtd": 0}]
+  allMoney = 0
+  users.forEach(user=>{
+    user.pending.forEach(item=>{
+      orders[item.id].qtd += item.qtd
+      allMoney += item.qtd*foods[item.id].preco
+    })
+  })
+}
+
+app.get('/', function (req, res){
+  res.write('index.html')
+  res.end()
+})
+app.get('/adm', function (req, res){
+  getAllOrder()
+  res.write(JSON.stringify(orders))
+  res.write("\n\n\n")
+  orders.forEach(order=>{
+    res.write(order.nome+": "+order.qtd+"\n")
+  })
+  res.write("\n\n\n")
+  res.write("Total: "+allMoney+"\n")
+  res.end()
+})
+app.get('/pagamento', function (req, res){
+  
+})
+
+
+
+server.listen(3000)
+
+io.on('connection', socket => {  
+  let id = socket.id
+  socket.on('login', (email, senha) => {
+    let found = false
+    let user
+    users.forEach(userF => {
+        if(userF.email==email && userF.senha == senha){
+            found = true
+            user = userF
+        }
+    })
+    if(found==true){
+      io.to(id).emit("loginState", {state: "Success", userInfo: user})
+    }else{
+      io.to(id).emit("loginState", {state: "Fail"})
+    }
+  })
+  socket.on('registrar', (email, senha) => {
+    let found = false
+    users.forEach(user => {
+      if(user.email==email){
+          found = true
+      }
+    })
+    if(found==true){
+      io.to(id).emit("regState", {state: "Fail"})
+    }else{
+      io.to(id).emit("regState", {state: "Success"})
+      users.push({"email": email, "senha":senha, "id":users.length+1, "saldo":0, "cart":[], "pending":[]})
+      fs.writeFile('users.json', JSON.stringify(users), (err) => {})
+    }
+  })
+  socket.on('addToCart', (id, amount, accountID) => {
+      let i = 0
+      let found = false
+      users.forEach(userF => {
+        if(userF.id == accountID){
+          let o = 0
+          users[i].cart.forEach(item => {
+            if(item.id == id){
+              users[i].cart[o].qtd+=amount
+              found = true
+            }
+            if(users[i].cart[o].qtd<1){
+              users[i].cart.pop(o)
+            }
+            o++
+          })
+          if(found == false){
+            users[i].cart.push({"id":id,"qtd":1})
+          }
+        }
+        i++
+      })
+      fs.writeFile('users.json', JSON.stringify(users), (err) => {})  
+  })
+  socket.on('askUser', (accountID) => {
+    users.forEach(user => {
+      if(user.id == accountID){
+        io.to(id).emit("updateUser", user)
+      }
+    })
+  })
+  socket.on("balancePayAsk", (accountID)=>{
+    let index = getUserIndex(accountID)
+    let total = 0
+    users[index].cart.forEach(item=>{
+      total+=item.qtd*foods[item.id].preco
+    })
+    if(users[index].saldo>=total){
+      users[index].saldo -=  total
+      io.to(id).emit("balancePayResp", true, users[index].saldo)
+      for (let i = 0; i < users[index].pending.length; i++) {
+        let found = 0
+        for (let o = 0; o < users[index].cart.length; o++) {
+          if(users[index].pending[i].id == users[index].cart[found].id){
+            users[index].pending[i].qtd+=users[index].cart[found].qtd
+            users[index].cart.splice(found, 1)
+          }else{
+            found++
+          }
+        }
+      }
+      
+      for (let p = 0; p <= users[index].cart.length; p++) {
+          if(users[index].cart[0]!=null){
+            users[index].pending.push(users[index].cart[0])
+          }  
+            users[index].cart.splice(0, 1)
+      }
+      io.to(id).emit("updateUser", users[index])
+      fs.writeFile('users.json', JSON.stringify(users), (err) => {})  
+    }else{
+      io.to(id).emit("balancePayResp", false, users[index].saldo)
+    }
+  })
+})
+
+async function a(){
+  setInterval(()=>{
+    if((new Date).getHours()==13){
+      users.forEach(user=>{
+        user.cart = []
+        user.pending = []
+      })
+    }
+  },6000000)
+}
+a()
