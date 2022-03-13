971f827df8b4bd745236a9ca466a006317db768b
Pessoadomundo <gabrieljotalizardo@gmail.com>
2022-03-12 21:00:59 -0300

    Update scriptAdm.js

--- public/scriptAdm.js
+++ public/scriptAdm.js
@@ -1,7 +1,7 @@
 const socket = io('74.207.230.69')
 
 document.getElementById("search").addEventListener("click", ()=>{
-    socket.emit("ADMaddMoney", document.getElementById("key"), document.getElementById("valor"))
+    socket.emit("ADMaddMoney", document.getElementById("key").value, document.getElementById("valor").value)
 })
 
 socket.on("ADMresult", resultado=>{
