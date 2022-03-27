c8c4ea0d5bd837a7751a60450f2c84af78765422
Pessoadomundo <gabrieljotalizardo@gmail.com>
2022-03-27 19:01:33 -0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -60,7 +60,7 @@ let mode = 1
 
 let tuTela = 0
 
-let tuTelas = [{"texto": "Passo 1: Criar a conta", "imagem": "tela1.mov"}, {"texto": "Passo 2: Fazer Login", "imagem": "tela2.mov"}, {"texto": "Passo 3: Adicionar Saldo", "imagem": "tela3.mov"}, {"texto": "Passo 4: Fazer Pedido", "imagem": "tela4.mov"}, {"texto": "", "imagem": "tela5.mov"}]
+let tuTelas = [{"texto": "Passo 1: Criar a conta", "imagem": "tela1.mov"}, {"texto": "Passo 2: Fazer Login", "imagem": "tela2.mov"}, {"texto": "Passo 3: Adicionar Saldo", "imagem": "tela3.mov"}, {"texto": "Passo 4: Fazer Pedido", "imagem": "tela4.mov"}, {"texto": "Fim", "imagem": "tela5.mov"}]
 
 
 function changePage(page){
@@ -161,6 +161,7 @@ function updateTut(tut){
         tutorialGif.play()
         tuText.innerHTML = tuTelas[tut].texto
         sauce.src = tuTelas[tut].imagem
+        window.scroll(0, 100);
     }
 }
 
