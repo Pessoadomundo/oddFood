3b1040227c97f04233af8e6bd1b66343fd42f028
Pessoadomundo <gabrieljotalizardo@gmail.com>
2022-05-22 18:49:12 -0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -201,7 +201,7 @@ function displayFood(id, type, elt, qtd=0){
         document.getElementById("foodInfoDiv").style.display = "block"
         document.getElementById("foodInfo").style.position = "fixed"
         document.getElementById("foodInfo").style.left = ""+(event.clientX-50)+"px"
-        document.getElementById("foodInfo").style.top = ""+(event.clientY-70)+"px"
+        document.getElementById("foodInfo").style.top = ""+(event.clientY-80)+"px"
     })
     eltDivImgComida.appendChild(eltImgComida)
     eltComida.appendChild(eltDivImgComida)
